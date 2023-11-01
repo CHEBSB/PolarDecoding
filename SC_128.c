2837459d7a81b679db469d006a91f3c58cd31a97
CHEBSB <44468299+CHEBSB@users.noreply.github.com>
2023-11-01 20:23:41 +0800

    Building encoder of polar code

---
+++ SC_128.c
@@ -0,0 +1,138 @@
+/*
+Simulation of successive cancellation (SC) decoder
+for polar code with N = 128 and rate = 0.5
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define bSNR_dB 3.046          // Eb/N0 in dB
+#define N 128
+#define K 64
+
+// seed for generating random number in (0, 1)
+const unsigned long long SEED = 1024; 
+unsigned long long RANV;
+int RANI = 0;
+double n1, n2;              // gaussian noise
+// standard deviation of Gaussian noise
+const double std = pow(10, bSNR_dB / ((double)-20)); 
+// 5G polar sequence (Q[0] is most unreliable)
+const int Q[N] = {
+0, 1, 2, 4, 8, 16, 32, 3, 5, 64, 9, 6, 17, 10, 18, 12, 33,
+65, 20, 34, 24, 36, 7, 66, 11, 40, 68, 19, 13, 48, 14, 72,
+21, 35, 26, 80, 37, 25, 22, 38, 96, 67, 41, 28, 69, 42, 49,
+74, 70, 44, 81, 50, 73, 15, 52, 23, 76, 82, 56, 27, 97, 39,
+84, 29, 43, 98, 88, 30, 71, 45, 100, 51, 46, 75, 104, 53,
+77, 54, 83, 57, 112, 78, 85, 58, 99, 86, 60, 89, 101, 31,
+90, 102, 105, 92, 47, 106, 55, 113, 79, 108, 59, 114, 87,
+116, 61, 91, 120, 62, 103, 93, 107, 94, 109, 115, 110, 117,
+118, 121, 122, 63, 124, 95, 111, 119, 123, 125, 126, 127};
+// n-kronecker product of F
+int **Fn; 
+
+// generate a uniform random number
+double Ranq1();            
+// generate 2 normal random number given standard deviation
+void normal();
+
+int main(void)
+{
+    int i, j, k;                // looping indices
+    int m = 0;                  // index for PN sequence
+    // stepsize of m after 1 iteration
+    int step_m = K % 63;                
+    // to generate PN sequence
+    int U[] = {0, 0, 0, 0, 0, 0};   // previous bits
+    int b;                      // current bit
+    int PN[63];                 // 1 period of PN sequence
+    int I[K];                   // information set
+    int errBlock = 0;           // number of block errors
+    int temp;                   // temporary storage
+    int w[K];                   // information bits
+    int u[N];                   // info bits + frozen bits
+    int x[N];                   // polar codeword
+    double y[N];                // codeword + Gaussian noise
+
+    // generate a cycle of PN sequence
+    for (i = 0; i < 63; i++) {
+        if (i == 0) b = 1;
+        else if (i < 6) b = 0;
+        else b = (U[4])? (!U[5]): U[5]; // exculsive or
+        PN[i] = b;                      // write the array
+        // flip-flop shift
+        U[5] = U[4];        
+        U[4] = U[3];
+        U[3] = U[2];
+        U[2] = U[1];
+        U[1] = U[0];
+        U[0] = b;
+    }
+    // determine the information set I
+    for (i = 0; i < K; i++) {
+        // pick most reliable channels
+        I[i] = Q[N - 1 - i];
+    }
+    // read Fn from file
+    Fn = (int **)calloc(N, sizeof(int *));
+    for (i = 0; i < N; i++) {
+        Fn[i] = (int *)calloc(N, sizeof(int));
+        for (j = 0; j < N; j++) {
+            scanf("%d", &temp);
+            if (temp != 0 && temp != 1)
+                printf("Illegal input!\n");
+            Fn[i][j] = temp;
+        }
+    }
+    printf("Fn init completed.\n");     // for debug
+    // init all bits in u to 0
+    for (i = 0; i < N; i++)
+        u[i] = 0;
+    // run simulation until desired error blocks
+    for (i = 0; errBlock < 50; i++) {
+        /* use PN sequence to have K bits, and
+        put them into information set in u */
+        for (j = 0; j < K; j++)
+            u[I[j]] = PN[(m + j) % 63];
+        // encode u to become x
+        // first, perform bit reversal
+        // then encode by Fn
+        m += step_m;                   // increase m
+        errBlock += 1;
+    }
+    return 0;
+}
+
+// generate an uniform random number in (0, 1)
+double Ranq1()
+{
+    if (RANI == 0) {
+        RANV = SEED ^ 4101842887655102017LL;
+        RANV ^= RANV >> 21;
+        RANV ^= RANV << 35;
+        RANV ^= RANV >> 4;
+        RANV = RANV * 2685821657736338717LL;
+        RANI++;
+    }
+    RANV ^= RANV >> 21;
+    RANV ^= RANV << 35;
+    RANV ^= RANV >> 4;
+    return RANV * 2685821657736338717LL * 5.42101086242752217E-20;
+}
+
+// generate 2 normal random number given standard deviation
+void normal()
+{
+    double x1, x2, s;       // x1 and x2 are Uniform(0, 1)
+
+    do {
+        x1 = Ranq1();
+        x2 = Ranq1();
+        x1 = 2 * x1 - 1;    // transform (0, 1) to (-1, 1)
+        x2 = 2 * x2 - 1;
+        s = x1 * x1 + x2 * x2;
+    } while (s >= 1.0);     // until s is in unit circle
+    n1 = std * x1 * sqrt(-2 * log(s) / s);
+    n2 = std * x2 * sqrt(-2 * log(s) / s);
+    return;
+}
